75161a7ad17d4c2746bb935cc420db95184add67
Fernando Costa <ferndcosta353@gmail.com>
2024-02-08 13:48:48 -0300

    fix: fix Story component index showing undefined
    fix Story componnent index showing undefined when called on Item.js to render an individual story

--- components/Story.js
+++ components/Story.js
@@ -2,7 +2,7 @@ export default function Story(story) {
     return `
         <div class="story">
             <div> 
-                <span class="gray">${story.index}</span>
+                <span class="gray">${story.index || ""}</span>
                 <span class="upvote">▲</span>
                 <a href="${story.url}">${story.title}</a>
                 <span>(${story.domain})</span>
@@ -12,7 +12,7 @@ export default function Story(story) {
                     ${story.points} points by ${story.user} ${story.time_ago}
                     |
                     <a href="#/item?id=${story.id}">
-                    ${story.comments_count} comments
+                        ${story.comments_count} comments
                     </a>
                     |
                     <span class="favorite">
