78d1adfcf01729e83d790b9aeb9a24dfa63a659c
Fernando Costa <ferndcosta353@gmail.com>
2024-02-09 17:43:51 -0300

    feat: add active link styles on path change to header links

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 import RouterHandler from './router.js';
+import setActiveLink from './utils/activeLink.js';
+
+window.onhashchange = () => {
+    setActiveLink();
+}
 
 class App {
     constructor() {
